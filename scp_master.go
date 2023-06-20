c9a1325ad59accd8627197e67b0ebba5a7d62d83
paulo.perez <paulo.perez@4braintech.com>
2023-06-20 11:58:02 -0300

    .

--- scp_master.go
+++ scp_master.go
@@ -589,7 +589,7 @@ func scp_get_alldata() {
 							area := math.Pi * math.Pow(bio_diametro/2000.0, 2)
 							dfloat := bio_v1_zero - float64(dint)
 							vol1 := (area * dfloat) / 1000.0
-							fmt.Println("DEBUG Volume ", b.BioreactorID, vol1)
+							fmt.Println("DEBUG Volume ", b.BioreactorID, dint, vol1)
 							if (vol1 >= 0) && (vol1 <= float64(bio_cfg[b.BioreactorID].Maxvolume)*1.2) {
 								bio[k].Volume = uint32(vol1)
 								level := (vol1 / float64(bio_cfg[b.BioreactorID].Maxvolume)) * 10
