fdfe6738f2de0e6c2c26e80bcd7adb9f9da792f2
paulo.perez <paulo.perez@4braintech.com>
2023-07-13 16:02:36 -0300

    .

--- scp_master.go
+++ scp_master.go
@@ -2528,9 +2528,12 @@ func scp_run_bio(bioid string) {
 		return
 	}
 	for bio[ind].Status != bio_die {
-		fmt.Println("\n\nQUEUE:", bio[ind].Queue)
-		fmt.Println("\nUNDOQUEUE:", bio[ind].UndoQueue)
-		fmt.Println("\nREDOQUEUE:", bio[ind].RedoQueue)
+		if len(bio[ind].Queue > 0) {
+			fmt.Println("\n\nBIO", bioid, " status", bio[ind].Status)
+			fmt.Println("\nQUEUE:", bio[ind].Queue)
+			fmt.Println("\nUNDOQUEUE:", bio[ind].UndoQueue)
+			fmt.Println("\nREDOQUEUE:", bio[ind].RedoQueue)
+		}
 
 		if len(bio[ind].Queue) > 0 && bio[ind].Status != bio_nonexist && bio[ind].Status != bio_error {
 			if bio[ind].Status != bio_pause || !bio[ind].MustPause && !bio[ind].MustStop {
