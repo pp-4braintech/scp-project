11f9a353b4d44f68e48855a9e9e29f8070f388ad
paulo.perez <paulo.perez@4braintech.com>
2023-07-15 09:51:51 -0300

    .

--- scp_master.go
+++ scp_master.go
@@ -109,7 +109,7 @@ const data_filename = "dumpdata"
 
 //const execpath = "/home/scpadm/scp-project/"
 const execpath = "./"
-const mainrouter = "192.168.0.1"
+const mainrouter = "192.168.0.5"
 const pingmax = 3
 
 const bio_nonexist = "NULL"
