0350c7817b8475f4fa6d3a6a854d9e5fc1b642f3
paulo.perez <paulo.perez@4braintech.com>
2023-07-17 16:56:29 -0300

    calculo de volume

--- scp_master.go
+++ scp_master.go
@@ -1330,14 +1330,11 @@ func scp_get_alldata() {
 								}
 							} else if bio[ind].Valvs[6] == 1 { // Carregando Agua
 								volc = float64(bio[ind].Volume)
-								if vol2 != -1 && vol2 > float64(bio[ind].Volume) && vol2 < float64(bio[ind].Volume)*1.2 {
+								if vol2 != -1 && (volc == 0 || (vol2 > float64(bio[ind].Volume) && vol2 < float64(bio[ind].Volume)*1.2)) {
 									volc = vol2
 								}
-								if vol1 != -1 && vol1 > float64(bio[ind].Volume) && vol1 < float64(bio[ind].Volume)*1.2 {
+								if vol1 != -1 && (volc == 0 || (vol1 > float64(bio[ind].Volume) && vol1 < float64(bio[ind].Volume)*1.2)) {
 									volc = vol1
-								}
-								if volc < float64(bio[ind].Volume) || volc > float64(bio[ind].Volume)*1.2 {
-									volc = float64(bio[ind].Volume)
 								}
 							} else {
 								if vol1 != -1 && vol2 != -1 {
