f3667aeaced7d7c2c51d4eb1e9fb068806e7852a
paulo.perez <paulo.perez@4braintech.com>
2023-07-24 11:33:44 -0300

    .

--- scp_master.go
+++ scp_master.go
@@ -3577,6 +3577,7 @@ func scp_process_conn(conn net.Conn) {
 				switch scp_device {
 				case scp_par_out:
 					outid := subparams[1]
+					fmt.Println("\n\nPAR OUT", bioid, outid)
 					if outid == "Descarte" {
 						outid = "DROP"
 					} else if outid == "Externo" {
