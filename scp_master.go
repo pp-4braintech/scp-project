22867266ffead428333582b56921ac526a215900
paulo.perez <paulo.perez@4braintech.com>
2023-07-31 11:42:20 -0300

    .

--- scp_master.go
+++ scp_master.go
@@ -1439,7 +1439,7 @@ func scp_get_volume(main_id string, dev_type string, vol_type string) (int, floa
 	cmd := "CMD/" + dev_addr + "/GET/" + vol_dev + "/END"
 	ret := scp_sendmsg_orch(cmd)
 	params := scp_splitparam(ret, "/")
-	fmt.Println("DEBUG SCP GET VOLUME: CMD", cmd, "\tRET", ret)
+	fmt.Println("DEBUG SCP GET VOLUME: CMD", cmd, "  RET", ret)
 	var volume float64
 	var dint int
 	volume = -1
