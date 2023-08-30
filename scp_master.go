8a49c78a4e26699a18d6a73a384b9ec973d924f8
paulo.perez <paulo.perez@4braintech.com>
2023-08-30 17:22:38 -0300

    .

--- scp_master.go
+++ scp_master.go
@@ -1854,6 +1854,8 @@ func scp_sync_functions() {
 	t_start_save := time.Now()
 	t_start_status := time.Now()
 	t_start_screens := time.Now()
+	t_start_screens_full := time.Now()
+
 	n_bio := 0
 	for {
 		if finishedsetup {
@@ -1872,8 +1874,14 @@ func scp_sync_functions() {
 			}
 
 			t_elapsed_screens := uint32(time.Since(t_start_screens).Seconds())
+			t_elapsed_screens_full := uint32(time.Since(t_start_screens_full).Seconds())
 			if t_elapsed_screens >= scp_refresscreens {
-				go scp_update_screen(bio[n_bio].BioreactorID, false)
+				if t_elapsed_screens_full > scp_refresscreens*12 {
+					scp_update_screen_times(bio[n_bio].BioreactorID)
+					t_start_screens_full = time.Now()
+				} else {
+					scp_update_screen(bio[n_bio].BioreactorID, false)
+				}
 				n_bio++
 				if n_bio >= len(bio) {
 					n_bio = 0
