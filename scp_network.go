635f06deee2b81edb9fde7aec54a58436aa53e02
paulo.perez <paulo.perez@4braintech.com>
2023-09-20 14:20:40 -0300

    .

--- scp_network.go
+++ scp_network.go
@@ -123,6 +123,7 @@ func scp_proxy(bfid string, r *http.Request) http.ResponseWriter {
 
 	// r.RequestURI = ""
 	r.URL.Scheme = "http"
+	r.URL.Path = fmt.Sprintf("http://%s:5000", bfs[ind].BFIP)
 	r.RemoteAddr = fmt.Sprintf("%s:5000", bfs[ind].BFIP)
 	client := &http.Client{}
 	delHopHeaders(r.Header)
