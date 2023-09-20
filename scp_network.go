9ee3ccd60941ad944b85040e7a2830c7f282b1f9
paulo.perez <paulo.perez@4braintech.com>
2023-09-20 16:30:17 -0300

    .

--- scp_network.go
+++ scp_network.go
@@ -46,7 +46,8 @@ var hopHeaders = []string{
 	"Upgrade",
 }
 
-var bfs = []Biofabrica_data{{"bf000", "HA", "Hubio Agro", "", "1.2.15", [2]float64{-18.9236672, -48.1827026}, "", "192.168.0.23"}}
+var bfs = []Biofabrica_data{{"bf000", "HA", "Hubio Agro", "", "1.2.15", [2]float64{-18.9236672, -48.1827026}, "", "192.168.0.23"},
+	{"bf001", "UG", "Unigeo", "", "1.2.15", [2]float64{-10.9236672, -38.1827026}, "", "192.168.0.23"}}
 
 func checkErr(err error) {
 	if err != nil {
