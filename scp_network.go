9d8d5f5db87848fea488e5f05abd5354f0c9d579
paulo.perez <paulo.perez@4braintech.com>
2023-09-20 09:35:57 -0300

    .

--- scp_network.go
+++ scp_network.go
@@ -7,6 +7,71 @@ import (
 	"github.com/rs/cors"
 )
 
+func main_network(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	fmt.Printf("Req: %s %s\n", r.Host, r.URL.Path)
+
+	// switch r.Method {
+	// case "GET":
+	// 	var jsonStr []byte
+	// 	totem_id := r.URL.Query().Get("Id")
+	// 	//fmt.Println("bio_id =", bio_id)
+	// 	//fmt.Println()
+	// 	if len(totem_id) > 0 {
+	// 		cmd := "GET/" + scp_totem + "/" + totem_id + "/END"
+	// 		jsonStr = []byte(scp_sendmsg_master(cmd))
+	// 	} else {
+	// 		cmd := "GET/" + scp_totem + "/END"
+	// 		jsonStr = []byte(scp_sendmsg_master(cmd))
+	// 	}
+	// 	//os.Stdout.Write(jsonStr)
+	// 	//jsonStr = []byte(scp_sendmsg_master(cmd))
+	// 	// os.Stdout.Write(jsonStr)
+	// 	w.Write([]byte(jsonStr))
+
+	// case "PUT":
+	// 	err := r.ParseForm()
+	// 	if err != nil {
+	// 		fmt.Println("ParseForm() err: ", err)
+	// 		return
+	// 	}
+	// 	// fmt.Println("Post from website! r.PostFrom = ", r.PostForm)
+	// 	// fmt.Println("Post Data", r.Form)
+	// 	totem_id := r.FormValue("Id")
+	// 	if len(totem_id) >= 0 {
+	// 		pump := r.FormValue("Pump")
+	// 		peris := r.FormValue("Perist")
+	// 		valve := r.FormValue("Valve")
+	// 		value_status := r.FormValue("Status")
+	// 		// fmt.Println("Pump = ", pump)
+	// 		// fmt.Println("Valve = ", valve)
+	// 		// fmt.Println("Status = ", valve_status)
+	// 		if pump != "" {
+	// 			cmd := "PUT/" + scp_totem + "/" + totem_id + "/" + scp_dev_pump + "," + pump + "/END"
+	// 			jsonStr := []byte(scp_sendmsg_master(cmd))
+	// 			// os.Stdout.Write(jsonStr)
+	// 			w.Write([]byte(jsonStr))
+	// 		}
+	// 		if valve != "" {
+	// 			cmd := "PUT/" + scp_totem + "/" + totem_id + "/" + scp_dev_valve + "," + valve + "," + value_status + "/END"
+	// 			jsonStr := []byte(scp_sendmsg_master(cmd))
+	// 			// os.Stdout.Write(jsonStr)
+	// 			w.Write([]byte(jsonStr))
+	// 		}
+	// 		if peris != "" {
+	// 			cmd := "PUT/" + scp_totem + "/" + totem_id + "/" + scp_dev_peris + "," + peris + "," + value_status + "/END"
+	// 			jsonStr := []byte(scp_sendmsg_master(cmd))
+	// 			// os.Stdout.Write(jsonStr)
+	// 			w.Write([]byte(jsonStr))
+	// 		}
+	// 	}
+
+	// default:
+
+	// }
+}
+
 func main() {
 
 	net192 = test_file("/etc/scpd/scp_net192.flag")
@@ -29,19 +94,19 @@ func main() {
 		AllowCredentials: false,
 	})
 
-	mux.HandleFunc("/", main_handler)
+	mux.HandleFunc("/", main_network)
 
-	mux.HandleFunc("/ibc_view", ibc_view)
+	// mux.HandleFunc("/ibc_view", ibc_view)
 
-	mux.HandleFunc("/totem_view", totem_view)
+	// mux.HandleFunc("/totem_view", totem_view)
 
-	mux.HandleFunc("/biofabrica_view", biofabrica_view)
+	// mux.HandleFunc("/biofabrica_view", biofabrica_view)
 
-	mux.HandleFunc("/simulator", biofactory_sim)
+	// mux.HandleFunc("/simulator", biofactory_sim)
 
-	mux.HandleFunc("/config", set_config)
+	// mux.HandleFunc("/config", set_config)
 
-	mux.HandleFunc("/wdpanel", withdraw_panel)
+	// mux.HandleFunc("/wdpanel", withdraw_panel)
 
 	handler := cors.Handler(mux)
 
