119ce228b4dd0cb521769655e902ee11e3186d27
paulo.perez <paulo.perez@4braintech.com>
2023-09-20 11:47:56 -0300

    .

--- scp_network.go
+++ scp_network.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -19,6 +20,7 @@ const (
 )
 
 var execpath string
+var bf_default string = "bf001"
 
 type Biofabrica_data struct {
 	BFId         string
@@ -69,6 +71,34 @@ func get_bf_index(bf_id string) int {
 	return -1
 }
 
+func scp_proxy(bfid string, endpoint string) string {
+	ind := get_bf_index(bfid)
+	if ind < 0 {
+		fmt.Println("ERROR SCP PROXY: Biofabrica nao encontrada", bfid)
+		return ""
+	}
+	bf_url := fmt.Sprintf("http://%s/%s", bfs[ind].BFIP, endpoint)
+	res, err := http.Get(bf_url)
+	fmt.Println("DEBUG RES=", res)
+	if err != nil {
+		checkErr(err)
+		return ""
+	}
+	// fmt.Println(res)
+	rdata, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		checkErr(err)
+		return ""
+	}
+	fmt.Println("rdata=", rdata)
+	return string(rdata)
+	// // fmt.Println(string(rdata))
+	// json.Unmarshal(rdata, &last_biofabrica)
+	// // fmt.Println(last_biofabrica)
+	// fmt.Println("DEBUG CHECK LASTVERSION: Ajustando ultima versão para:", last_biofabrica.Version)
+	// biofabrica.LastVersion = last_biofabrica.Version
+}
+
 func main_network(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
@@ -88,11 +118,21 @@ func main_network(w http.ResponseWriter, r *http.Request) {
 			params := scp_splitparam(endpoint, "/")
 			fmt.Println("end=", params, len(params))
 			if len(params) > 2 {
-				bfid := params[1]
-				ind := get_bf_index(bfid)
-				if ind < 0 {
-					w.Write([]byte(scp_err))
+				cmd := params[1]
+				if cmd == "bf_default" {
+					ind := get_bf_index(bf_default)
+					if ind < 0 {
+						w.Write([]byte(scp_err))
+					} else {
+						jsonStr, _ = json.Marshal(bfs)
+						// os.Stdout.Write(jsonStr)
+						w.Write([]byte(jsonStr))
+					}
+				} else {
+					ret := scp_proxy(bf_default, endpoint)
+					w.Write([]byte(ret))
 				}
+
 			}
 		}
 		// totem_id := r.URL.Query().Get("Id")
