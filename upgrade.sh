e4f42a9ce66c8193be873dba1951ed3dabdc5a9d
paulo.perez <paulo.perez@4braintech.com>
2023-09-22 16:16:28 -0300

    .

--- upgrade.sh
+++ upgrade.sh
@@ -12,7 +12,7 @@ fi
 cd /home/scpadm/scp-project
 
 cp scp_*.go /tmp/
-rm scp_master scp_orch scp_back
+rm scp_master scp_orch scp_back scp_agent
 git config --global --add safe.directory /home/scpadm/scp-project
 git stash
 git pull
@@ -21,6 +21,20 @@ if [ $? -ne 0 ]
         echo "Nao foi possivel fazer a atualizacao"
         cp /tmp/scp_*.go . 
         exit 1
+fi
+
+DIR=/etc/systemd/system/
+FILE=scp_agent.service
+
+if [ -e "$DIR$FILE" ] 
+    then
+        echo "SCP AGENT OK"
+
+    else
+        echo "Criando SCP AGENT"
+        cp /home/scpadm/scp-project/inid/scp_agent.service /etc/systemd/system/
+        systemctl enable scp_agent
+        systemctl start scp_agent
 fi
 
 echo "Atualizando Front End"
@@ -41,6 +55,10 @@ systemctl restart scp_orch
 echo "Restartando Back End"
 go build scp_back.go
 systemctl restart scp_back
+
+echo "Restartando Back Agent"
+go build scp_agent.go
+systemctl restart scp_agent
 
 echo "Restardando Master"
 go build scp_master.go
