51fbb0e443343508b8f0d859925a0254ea41a553
paulo.perez <paulo.perez@4braintech.com>
2023-10-16 12:25:35 -0300

    .

--- scp_slaveusb.go
+++ scp_slaveusb.go
@@ -4,12 +4,35 @@ import (
 	"encoding/hex"
 	"fmt"
 	"log"
+	"net"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
+const firm_version = "SLAVEUSB_V1.0.0";
+
+const scp_slave_udpport = "9009"
+const scp_slave_tcpport = "9119"
+const scp_master_port = "7007"
+const scp_retries = 10
+const scp_keepalive_time = 10
+
+const scp_ack = "ACK"
+const scp_join = "JOIN"
+const scp_end = "END"
+const scp_put = "PUT"
+const scp_get = "GET"
+const scp_mod = "MOD"
+const scp_inv = "INV"
+const scp_ierr = "IERR"
+const scp_ping = "PING"
+
 var scp_slave_addr string
+var scp_connected = false
+
+var scp_last_IP net.Addr
 
 func checkErr(err error) {
 	if err != nil {
@@ -72,9 +95,58 @@ func mac2scpaddr(mac string) string {
 	return string(scpaddr)
 }
 
+func scp_sendudp(*con net.PacketConn,scp_dest_addr net.Addr, scp_message []byte, scp_len int, wait_ack bool) bool {
+	var buf [2048]byte
+	
+	has_ack := !wait_ack
+	for ntries:=0; ntries<scp_retries; ntries++ {
+		_,err := con.Writeto(scp_message, scp_dest_addr)
+		checkErr(err)
+		for reps:=0; !has_ack && reps<scp_retries; reps++ {
+			packetSize, err := con.Read(buf[0:])
+			if err != nil {
+				checkErr(err)
+			} else {
+				scp_last_IP = con.RemoteAddr()
+				fmt.Println("Remote addr =",scp_last_ip," dados =",buf)
+				if strings.Contains(string(buf),scp_ack) {
+					has_ack = true
+				}
+			}
+		}
+		if has_ack{
+			break
+		}
+		time.Sleep(1 * time.Second)
+	} 
+	return has_ack
+  }
+
+
+func scp_join_server() bool {
+	con, err := net.ListenPacket("udp4", ":"+scp_slave_udpport)
+	if err!= nil {
+		fmt.Println("ERROR SCP JOIN SERVER: Nao foi possivel criar o socket UDP")
+		checkErr(err)
+		return false
+	}
+	defer con.Close()
+	broadcast_addr, err := net.ResolveUDPAddr("udp4", "10.0.0.255:"+scp_master_port)
+	if err!= nil {
+		fmt.Println("ERROR SCP JOIN SERVER: Nao foi possivel determinar endereco do Master")
+		checkErr(err)
+		return false
+	}
+	// hasjoin := false
+	// ntries := 0
+	join_msg := fmt.Sprintf("%s/%s/%u/%s/%s", scp_join, scp_slave_addr, scp_slave_tcpport, firm_version, scp_end)
+	scp_sendudp(&con, broadcast_addr, []byte(join_msg), len(join_msg), true)
+
+}
+
 func scp_setup_slave() {
 	mymac := get_eth_mac()
-	fmt.Println("My mac =", mymac)
+	fmt.Println("DEBUG SCP SETUP SLAVE: My MAC =", mymac)
 	tmpaddr := mac2scpaddr(mymac)
 	if len(tmpaddr) > 0 {
 		scp_slave_addr = tmpaddr
