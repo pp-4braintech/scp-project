652a5191ca1320aa8c9b44c7dbed4a528ac3f605
pp-4braintech <paulo.perez@4braintech.com>
2024-08-19 20:17:54 -0300

    .

--- upgrade_sys.sh
+++ upgrade_sys.sh
@@ -28,18 +28,36 @@ sudo chpasswd < alt.pass
 
 cp initd/_bashrc /home/scpadm/.bashrc
 
-DIR=/etc/systemd/system/
-FILE=scp_agent.service
+DIRSYS=/etc/systemd/system/
+FILEAGENT=scp_agent.service
 
-if [ -e "$DIR$FILE" ] 
+if [ -e "$DIRSYS$FILEAGENT" ] 
     then
         echo "SCP AGENT OK"
-
     else
         echo "Criando SCP AGENT"
         cp /home/scpadm/scp-project/initd/scp_agent.service /etc/systemd/system/scp_agent.service
         systemctl enable scp_agent
         systemctl start scp_agent
+fi
+
+DIRETC=/etc/scpd/
+FILESTDA=scp_standalone.flag
+FILESERV=scp_slaveusb.service
+
+if [ -e "$DIRETC$FILESTDA" ] 
+    then
+        if [ -e "$DIRSYS$FILESERV" ] 
+            then
+                echo "STANDALONE SCP SLAVEUSB OK"
+            else
+                echo "STANDALONE Criando SCP SLAVEUSB"
+                cp /home/scpadm/scp-project/initd/scp_slaveusb.service /etc/systemd/system/scp_slaveusb.service
+                systemctl enable scp_slaveusb
+                systemctl start scp_slaveusb
+        fi
+    else 
+        echo "MODO BIOFABRICA"
 fi
 
 echo "Atualizando Front End"
@@ -55,6 +73,14 @@ cd /home/scpadm/scp-project
 
 echo "Restartando Orquestrador"
 systemctl restart scp_orch
+
+if [ -e "$DIRETC$FILESTDA" ] 
+    then
+        echo "Restartando SlaveUSB"
+        systemctl restart scp_slaveusb
+    else 
+        echo "OK"
+fi
 
 echo "Restartando Back End"
 systemctl restart scp_back
