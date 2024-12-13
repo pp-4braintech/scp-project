d398579c9e3808cd0dfe56389aabf5a79201eb5d
paulo.perez <paulo.perez@4braintech.com>
2024-12-13 17:40:41 -0300

    bug firefox

--- upgrade.sh
+++ upgrade.sh
@@ -44,6 +44,9 @@ FILESERV=scp_slaveusb.service
 
 if [ -e "$DIRETC$FILESTDA" ] 
     then
+	echo "STANDALONE COPIANDO START BROWSE"
+	cp /home/scpadm/scp-project/initd/start_browser.sh /home/scpadm/scp-project/
+
         if [ -e "$DIRSYS$FILESERV" ] 
             then
                 echo "STANDALONE SCP SLAVEUSB OK"
